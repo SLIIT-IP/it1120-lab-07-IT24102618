2e2c51d4b4c94454fb8949f7788fa074604cf53c
IT24102618 <it24102618@my.sliit.lk>
2024-09-21 10:14:58 +0530

    Add files via upload

---
+++ Q1/IT24102618Lab7Q1B.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class IT24102618Lab7Q1B{
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+int numofst=3;
+for(int i=1; i<=numofst;i++){
+System.out.println("Student" +i);
+System.out.println("Enter marks of four subjects:");
+System.out.print("Enter Subject Mark 1:");
+int M1 = scanner.nextInt();
+System.out.print("Enter Subject Mark 2:");
+int M2 = scanner.nextInt();
+System.out.print("Enter Subject Mark 3:");
+int M3 = scanner.nextInt();
+System.out.print("Enter Subject Mark 4:");
+int M4 = scanner.nextInt();
+double avg = (M1+M2+M3+M4)/4.0;
+String grade = "";
+if((avg>=75)&&(avg<=100)){
+grade = "Distinction";
+}
+else if((avg>=50)&&(avg<=74)){
+grade = "Credit";
+}
+else if((avg>=0)&&(avg<=49)){
+grade = "Fail";
+}
+else{
+System.out.println("enter the correct marks");
+}
+System.out.println("Average is:" +avg);
+System.out.println("Overall Grade is:" +grade);
+System.out.println("");
+}
+
+scanner.close();
+}
+}
+
+
+
+
+
